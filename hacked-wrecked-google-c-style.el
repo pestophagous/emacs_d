9262f2b8729b674381fd4012519b5064b851b371
pestophagous <pestophagous@users.noreply.github.com>
2015-10-20 16:31:25 -0700

    the old my-c-mode-common-hook and replacing it with hacked-personalized-perhaps-broken-ssstyle. the crowning achievement is defun feedargs-lineup-expression-plus-4, which makes it so that formatting using emacs will not *fight* *with* and counteract use of clang-format at the command-line

--- hacked-wrecked-google-c-style.el
+++ hacked-wrecked-google-c-style.el
@@ -1,31 +1,5 @@
-;;; google-c-style.el --- Google's C/C++ style for c-mode
+;;; completely HACKED-UP and personalized file that BEGAN life as  google-c-style.el
 
-;; Keywords: c, tools
-
-;; google-c-style.el is Copyright (C) 2008 Google Inc. All Rights Reserved.
-;;
-;; It is free software; you can redistribute it and/or modify it under the
-;; terms of either:
-;;
-;; a) the GNU General Public License as published by the Free Software
-;; Foundation; either version 1, or (at your option) any later version, or
-;;
-;; b) the "Artistic License".
-
-;;; Commentary:
-
-;; Provides the google C/C++ coding style. You may wish to add
-;; `google-set-c-style' to your `c-mode-common-hook' after requiring this
-;; file. For example:
-;;
-;;    (add-hook 'c-mode-common-hook 'google-set-c-style)
-;;
-;; If you want the RETURN key to go to the next line and space over
-;; to the right place, add this to your .emacs right after the load-file:
-;;
-;;    (add-hook 'c-mode-common-hook 'google-make-newline-indent)
-
-;;; Code:
 
 ;; For some reason 1) c-backward-syntactic-ws is a macro and 2)  under Emacs 22
 ;; bytecode cannot call (unexpanded) macros at run time:
@@ -47,7 +21,7 @@ regardless of the number of nested parentheses, but excluding
 non-expression material such as \"if(\" and \"for(\" control
 structures.
 
-Suitable for inclusion in `c-offsets-alist'."
+Suitable for inclusion in 'c-offsets-alist'."
   (save-excursion
     (back-to-indentation)
     ;; Go to beginning of *previous* line:
@@ -64,88 +38,129 @@ Suitable for inclusion in `c-offsets-alist'."
       (goto-char (match-end 0))))
     (vector (+ 4 (current-column)))))
 
-;;;###autoload
-(defconst google-c-style
-  `((c-recognize-knr-p . nil)
-    (c-enable-xemacs-performance-kludge-p . t) ; speed up indentation in XEmacs
-    (c-basic-offset . 2)
-    (indent-tabs-mode . nil)
-    (c-comment-only-line-offset . 0)
-    (c-hanging-braces-alist . ((defun-open after)
-                               (defun-close before after)
-                               (class-open after)
-                               (class-close before after)
-                               (inexpr-class-open after)
-                               (inexpr-class-close before)
-                               (namespace-open after)
-                               (inline-open after)
-                               (inline-close before after)
-                               (block-open after)
-                               (block-close . c-snug-do-while)
-                               (extern-lang-open after)
-                               (extern-lang-close after)
-                               (statement-case-open after)
-                               (substatement-open after)))
-    (c-hanging-colons-alist . ((case-label)
-                               (label after)
-                               (access-label after)
-                               (member-init-intro before)
-                               (inher-intro)))
-    (c-hanging-semi&comma-criteria
-     . (c-semi&comma-no-newlines-for-oneline-inliners
-        c-semi&comma-inside-parenlist
-        c-semi&comma-no-newlines-before-nonblanks))
-    (c-indent-comments-syntactically-p . t)
-    (comment-column . 40)
-    (c-indent-comment-alist . ((other . (space . 2))))
-    (c-cleanup-list . (brace-else-brace
-                       brace-elseif-brace
-                       brace-catch-brace
-                       empty-defun-braces
-                       defun-close-semi
-                       list-close-comma
-                       scope-operator))
-    (c-offsets-alist . ((arglist-intro google-c-lineup-expression-plus-4)
-                        (func-decl-cont . ++)
-                        (member-init-intro . ++)
-                        (inher-intro . ++)
-                        (comment-intro . 0)
-                        (arglist-close . c-lineup-arglist)
-                        (topmost-intro . 0)
-                        (block-open . 0)
-                        (inline-open . 0)
-                        (substatement-open . 0)
-                        (statement-cont
-                         .
-                         (,(when (fboundp 'c-no-indent-after-java-annotations)
-                             'c-no-indent-after-java-annotations)
-                          ,(when (fboundp 'c-lineup-assignments)
-                             'c-lineup-assignments)
-                          ++))
-                        (label . /)
-                        (case-label . +)
-                        (statement-case-open . +)
-                        (statement-case-intro . +) ; case w/o {
-                        (access-label . /)
-                        (innamespace . 0))))
-  "Google C/C++ Programming Style.")
+
+(defun feedargs-lineup-expression-plus-4 (langelem)
+  "Indents to the beginning of the current C expression plus 4 spaces.
+
+Suitable for inclusion in 'c-offsets-alist'."
+  (save-excursion
+    (back-to-indentation)
+
+    (c-beginning-of-statement-1)
+
+    (back-to-indentation)
+
+    (vector (+ 4 (current-column)))
+) ; end of save-excursion
+); end of defun
+
+; http://emacswiki.org/emacs/IndentingC
+
 
 ;;;###autoload
-(defun google-set-c-style ()
-  "Set the current buffer's c-style to Google C/C++ Programming
-  Style. Meant to be added to `c-mode-common-hook'."
-  (interactive)
-  (make-local-variable 'c-tab-always-indent)
-  (setq c-tab-always-indent t)
-  (c-add-style "Google" google-c-style t))
+(defun hacked-personalized-perhaps-broken-ssstyle ()
+  (setq c-recognize-knr-p  nil)
+  (setq c-enable-xemacs-performance-kludge-p  t) ; speed up indentation in XEmacs
+
+  (setq c++-tab-always-indent t)
+  (setq c-basic-offset 4) ;; Default is 2
+  (setq c-indent-level 4) ;; Default is 2
+  (setq tab-width 4)
+  (c-toggle-hungry-state 1)
+
+  (modify-syntax-entry ?_ "w")
+  (set-fill-column 100)
+					;(comment-column . 40) ;  need to test
+
+  (setq c-indent-comment-alist
+	(append '((other . (space . 2))) c-indent-comment-alist))
+		  ;(end-block . (space . 1))
+
+  (setq indent-tabs-mode  nil)
+  (font-lock-add-keywords
+   nil '( ;;  new C++11 keywords. (each string literal is a REGEX)
+	 ("\\<\\(alignof\\|alignas\\|constexpr\\|decltype\\|noexcept\\|nullptr\\|static_assert\\|thread_local\\|override\\|final\\)\\>" . font-lock-keyword-face)
+	 ("\\<\\(char16_t\\|char32_t\\)\\>" . font-lock-keyword-face)
+	 ;; user-defined types (rather project-specific) (kept as an example of how to do this)
+	 ("\\<\\(xstring\\|xchar\\)\\>" . font-lock-type-face)
+	 ))
+  (setq c-comment-only-line-offset  0)
+  (setq c-hanging-braces-alist
+	(append '((defun-open after)
+		  (defun-close before after)
+		  (class-open after)
+		  (class-close before after)
+		  (inexpr-class-open after)
+		  (inexpr-class-close before)
+		  (namespace-open after)
+		  (inline-open after)
+		  (inline-close before after)
+		  (block-open after)
+		  (block-close . c-snug-do-while)
+		  (extern-lang-open after)
+		  (extern-lang-close after)
+		  (statement-case-open after)
+		  (substatement-open after)) c-hanging-braces-alist))
+  (setq c-hanging-colons-alist
+	(append '((case-label)
+		  (label after)
+		  (access-label after)
+		  (member-init-intro before)
+		  (inher-intro)) c-hanging-colons-alist))
+
+  (setq c-hanging-semi&comma-criteria
+	(append '(c-semi&comma-no-newlines-for-oneline-inliners
+		  c-semi&comma-inside-parenlist
+		  c-semi&comma-no-newlines-before-nonblanks) c-hanging-semi&comma-criteria))
+
+
+  (setq c-indent-comments-syntactically-p  t)
+
+  (setq c-cleanup-list
+	(append '(brace-else-brace
+		  brace-elseif-brace
+		  brace-catch-brace
+		  empty-defun-braces
+		  defun-close-semi
+		  list-close-comma
+		  scope-operator) c-cleanup-list))
+
+  (setq c-offsets-alist
+	(append '((arglist-intro google-c-lineup-expression-plus-4)
+		  (arglist-cont-nonempty feedargs-lineup-expression-plus-4)
+		  (func-decl-cont . ++)
+		  (member-init-intro . ++)
+		  (inher-intro . ++)
+		  (comment-intro . 0)
+		  (arglist-close . c-lineup-arglist)
+		  (topmost-intro . 0)
+		  (block-open . 0)
+		  (inline-open . 0)
+		  (substatement-open . 0)
+		  (statement-cont
+		   .
+		   (,(when (fboundp 'c-no-indent-after-java-annotations)
+		       'c-no-indent-after-java-annotations)
+		    ,(when (fboundp 'c-lineup-assignments)
+		       'c-lineup-assignments)
+		    ++))
+		  (label . /)
+		  (case-label . +)
+		  (statement-case-open . +)
+		  (statement-case-intro . +) ; case w/o {
+		  (access-label . /)
+		  (innamespace . 0)) c-offsets-alist))
+) ;; end defun
+
+
 
 ;;;###autoload
 (defun google-make-newline-indent ()
   "Sets up preferred newline behavior. Not set by default. Meant
-  to be added to `c-mode-common-hook'."
+  to be added to 'c-mode-common-hook'."
   (interactive)
   (define-key c-mode-base-map "\C-m" 'newline-and-indent)
   (define-key c-mode-base-map [ret] 'newline-and-indent))
 
-(provide 'google-c-style)
+(provide 'hacked-wrecked-google-c-style)
 ;;; google-c-style.el ends here
